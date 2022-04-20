e60ba9f9d78d21497d69ce1857f49b98931b013a
KMV <9117479@gmail.com>
2022-04-20 17:08:41 +0300

    NS BottomNavigationView Lesson29fin

--- app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.bottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.bottomnavigationview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bnView.selectedItemId = R.id.news
+        binding.bnView.setOnItemSelectedListener  {
+            when(it.itemId){
+                R.id.afisha -> {
+                    Toast.makeText(this, "Afisha", Toast.LENGTH_SHORT).show()
+                }
+                R.id.audio -> {
+                    Toast.makeText(this, "audio", Toast.LENGTH_SHORT).show()
+                }
+                R.id.map -> {
+                    Toast.makeText(this, "map", Toast.LENGTH_SHORT).show()
+                }
+                R.id.news -> {
+                    Toast.makeText(this, "news", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
